aacf7513244cea31c0874c7189fd3aa3ce1ce575
ModulaStudio <fatmasood01@gmail.com>
2025-08-01 16:50:18 +0300

    commit 2nd day tasks

---
+++ Day 2/tasks.py
@@ -0,0 +1,18 @@
+print("Hello"[0]) #subscripting
+print(len("Hello")) #length of the string
+print("123" + "456") #concatenation 
+print(12345678)  #large integer
+print("Hello"[1:4]) #slicing
+print("Hello" * 3) #repetition    
+print(type("Hello")) #type of the string
+int() #conversion to integer
+float() #conversion to float
+str() #conversion to string
+bool() #conversion to boolean
+# print("Number of letters in your name:" + str(len(input("Enter your name: ")))) #input and length 
+print(6 // 3) #integer division
+print(2 ** 2) #exponentiation
+print(round(3.7698989, 2))  #rounding a number
+print(f"Hello, {9}")  #formatted string literal (f-string)
+a = int("5") / int(2.7)
+print(type(a))  #type of the variable a
