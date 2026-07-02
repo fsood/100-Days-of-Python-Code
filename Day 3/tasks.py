aacd3b4f8af6dc6499ebe3b42b64f2485030e9b4
fsood <fsood53@gmail.com>
2026-07-02 23:21:59 +0400

    commit new changes

---
+++ Day 3/tasks.py
@@ -0,0 +1,25 @@
+# if condition:
+#  do this
+# else:
+#  do this
+
+# Nested If statements are used to check multiple conditions. The syntax is as follows:
+# if condition1:
+#     # do this
+#     x = something
+#     if condition2:
+#         # do this
+#     else:
+#         # do this
+# else:
+#     # do this
+
+# Elif statements are used to check multiple conditions. The syntax is as follows:
+# if condition1:  
+#     # do this
+# elif condition2:
+#     # do this
+# elif condition3:
+#     # do this
+# else:
+#     # do this
